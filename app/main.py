06e76786253f66983904286007d9ebc6d6863237
Aditya-Yadav-1 <adityarao247@gmail.com>
2024-12-03 12:19:46 +0530

    get / route added

--- app/main.py
+++ app/main.py
@@ -4,3 +4,7 @@ from routes.student_routes import router as student_router
 app = FastAPI()
 
 app.include_router(student_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Student Management App!"}
